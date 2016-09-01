3ab43cad8e840225f23a66196f05a770bbcce0c2
874663394 <874663394@qq.com>
2016-09-01 11:27:42 +0800

    添加了客户信息查询方法和之前方法的解密table操作

---
+++ BaseLayer/Base/ClientBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLayer.Base
+{
+    public class ClientBase
+    {
+        /// <summary>
+        /// 查询所有客户信息
+        /// </summary>
+        /// <param name="isflag">true显示全部 false显示未禁用</param>
+        /// <returns></returns>
+        public DataTable SelClient(bool isflag)
+        {
+            string sql = "";
+            DataSet ds = null;
+            try
+            {
+                sql = "select * from T_BaseClient";
+                if (isflag == false)
+                {
+                    sql += " where enable=1";
+                }
+            }
+            catch
+            {
+                throw new Exception("-1");
+            }
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, DbHelperSQL.connectionString);
+                ds = new DataSet();
+                adapter.Fill(ds, "T_BaseClient");
+            }
+            catch
+            {
+                throw new Exception("-2");
+            }
+            return ds.Tables[0];
+        }
+    }
+}
---
+++ LogicLayer/Base/ClientLogic.cs
@@ -0,0 +1,26 @@
+﻿using BaseLayer.Base;
+using HelperUtility.Encrypt;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.Base
+{
+    public class ClientLogic
+    {
+        ClientBase cb = new ClientBase();
+        CodingHelper ch = new CodingHelper();
+        /// <summary>
+        /// 查询所有客户信息
+        /// </summary>
+        /// <param name="isflag">true显示全部 false显示未禁用</param>
+        /// <returns></returns>
+        public DataTable SelClient(bool isflag)
+        {
+            return ch.DataTableReCoding(cb.SelClient(isflag));
+        }
+    }
+}
