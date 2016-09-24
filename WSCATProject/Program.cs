7808b07aac550272490b0f319f3ac79537151e1b
linmusi <137366501@qq.com>
2016-09-24 18:03:48 +0800

    2121212

--- WSCATProject/Program.cs
+++ WSCATProject/Program.cs
@@ -14,7 +14,7 @@ namespace WSCATProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           Application.Run(new WareHouseInventoryForm());
+           Application.Run(new WareHouseOutMainForm());
         }
     }
 }
