d9b2e30db2c30f25e166da7a3baf7c60a18e91c6
874663394 <874663394@qq.com>
2016-09-28 14:17:11 +0800

    123213

---
+++ BaseLayer/Base/MaterialBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLayer.Base
+{
+    public class MaterialBase
+    {
+        public int SetMaterialNumber(string materialCode, string number)
+        {
+            string sql = "";
+            int result = 0;
+            try
+            {
+                sql = string.Format(@"update T_BaseMaterial set price={0} where code='{1}')", number, materialCode);
+                result = DbHelperSQL.ExecuteSql(sql);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result;
+        }
+    }
+}
---
+++ LogicLayer/Base/MaterialLogic.cs
@@ -0,0 +1,58 @@
+﻿using BaseLayer;
+using BaseLayer.Base;
+using HelperUtility;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpdateManagerLayer;
+
+namespace LogicLayer.Base
+{
+    public class MaterialLogic
+    {
+        MaterialBase _dal = new MaterialBase();
+        LogBase _logDal = new LogBase();
+        BaseUpdataManager baseUpdate = new BaseUpdataManager();
+        public int SetMaterialNumber(string materialCode, string number)
+        {
+            int result = 0;
+            Log model = new Log()
+            {
+                code = BuildCode.ModuleCode("log"),
+                operationCode = "操作人code",
+                operationName = "操作人名",
+                operationTable = "T_BaseMaterial",
+                operationTime = DateTime.Now,
+                objective = "修改物料信息",
+                operationContent = "修改T_BastMaiterial表的数据,条件:materialCode=" + materialCode
+            };
+            try
+            {
+                if ((materialCode == null || materialCode == "") && string.IsNullOrWhiteSpace(number))
+                {
+                    throw new Exception("-2");
+                }
+                result = _dal.SetMaterialNumber(materialCode, number);
+                if (result <= 0)
+                {
+                    throw new Exception("-3");
+                }
+                model.result = 1;
+                baseUpdate.add(materialCode, model.operationTable, result, "", model.operationTime);
+            }
+            catch (Exception ex)
+            {
+                model.result = 0;
+                throw ex;
+            }
+            finally
+            {
+                _logDal.Add(model);
+            }
+            return result;
+        }
+    }
+}
