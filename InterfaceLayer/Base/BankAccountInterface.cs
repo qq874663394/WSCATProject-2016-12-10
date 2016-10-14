569cdb9147da5962d23e320a5f7a4e1833483141
874663394 <874663394@qq.com>
2016-10-14 09:29:21 +0800

    123123

--- InterfaceLayer/Base/BankAccountInterface.cs
+++ InterfaceLayer/Base/BankAccountInterface.cs
@@ -14,7 +14,7 @@ namespace InterfaceLayer.Base
         /// <summary>
         /// 获得数据列表
         /// </summary>
-        /// <param name="fieldName">条件字段名,0:code 1:模糊查询持卡人</param>
+        /// <param name="fieldName">条件字段名,0:code 1:模糊查询持卡人 2:模糊查询卡号</param>
         /// <param name="fieldValue">条件值</param>
         /// <param name="isClear">是否检索所有删除状态,true:检索已删除和未删除的数据，false:只检索未删除的数据</param>
         /// <param name="isEnable">是否检索所有禁用状态,true:检索已禁用和未禁用的数据，false:只检索未禁用的数据</param>
