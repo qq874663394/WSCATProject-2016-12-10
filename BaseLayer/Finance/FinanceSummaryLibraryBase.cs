abc589c9b63ced8142e685c021e9623ab9ed0910
yyq <123456@qq.com>
2016-11-24 10:58:27 +0800

    8888

---
+++ BaseLayer/Finance/FinanceSummaryLibraryBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLayer.Finance
+{
+    class FinanceSummaryLibraryBase
+    {
+        /// <summary>
+        /// 获取最新的code
+        /// </summary>
+        /// <returns></returns>
+        public string AddParentNode(string name)
+        {
+            string code = 
+            string sql = string.Format("insert into [T_FinanceSummaryLibrary] (code,name) values('{0}','{1}')",,name);
+            return DbHelperSQL.GetSingle(sql).ToString();
+        }
+    }
+}
---
+++ WSCATProject/Finance/FinanceCategoriesDialogForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WSCATProject.Finance
+{
+    public partial class FinanceCategoriesDialogForm : Form
+    {
+        public FinanceCategoriesDialogForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FinanceCategoriesDialogForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Dispose();
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            string name; //类别名称
+            try
+            {
+                name = this.txtName.Text;
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    MessageBox.Show("请在输入值后再进行添加操作！");
+                    return;
+                }
+                else
+                {
+                    //传值
+
+                }
+            }
+            catch
+            {
+                //执行失败
+            }
+                this.Close();
+                this.Dispose();
+        }
+    }
+}
