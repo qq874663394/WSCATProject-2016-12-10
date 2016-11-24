08ea4eb1ec5d0feae74a04d0768b804de94e82c7
874663394 <874663394@qq.com>
2016-11-24 10:55:42 +0800

    123213213

--- HelperUtility/ExceptionTIP.cs
+++ HelperUtility/ExceptionTIP.cs
@@ -8,9 +8,44 @@ namespace HelperUtility
 {
     public class ExceptionTIP
     {
-        public string ss()
+        public string TIPcatch(string number)
         {
+            switch (number)
+            {
+                case "-1":
 
+                    break;
+                case "-2":
+
+                    break;
+                case "-3":
+
+                    break;
+                case "-4":
+
+                    break;
+                case "-5":
+
+                    break;
+                case "-6":
+
+                    break;
+                case "-7":
+
+                    break;
+                case "-8":
+
+                    break;
+                case "-9":
+
+                    break;
+                case "-0":
+
+                    break;
+                default:
+                    break;
+            }
+            return "";
         }
     }
 }
